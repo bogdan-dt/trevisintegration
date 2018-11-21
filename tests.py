2081655606df6cbdbc8d2137c8dffa30470fd8b1
th3librarian <bog.tiron@yahoo.com>
2018-11-21 11:15:46 +0000

    first commit with unittests

---
+++ tests.py
@@ -0,0 +1,23 @@
+import unittest
+
+from mymathlib import is_prime
+
+
+class IsPrimeTests(unittest.TestCase):
+    def test_zero(self):
+            self.assertFalse(is_prime(0))
+
+    def test_one(self):
+            self.assertFalse(is_prime(1))
+
+    def test_two(self):
+            self.assertTrue(is_prime(2))
+
+    def test_three(self):
+            self.assertTrue(is_prime(3))
+
+    def test_four(self):
+            self.assertFalse(is_prime(4))
+
+if __name__ == "__main__":
+        unittest.main()
